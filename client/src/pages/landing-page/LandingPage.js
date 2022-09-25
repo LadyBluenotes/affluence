907abee149d45650ffca5ed5f32ae29599eaa15c
Sarah Gerrard <98355961+LadyBluenotes@users.noreply.github.com>
2022-09-25 13:29:34 -0700

    first commit

---
+++ client/src/pages/landing-page/LandingPage.js
@@ -0,0 +1,11 @@
+import { Typography } from '@mui/material';
+import * as React from 'react';
+import LandingNav from '../../components/landingNav';
+
+export default function LandingPage() {
+  return (
+    <>
+    <LandingNav />
+    </>
+  );
+}
