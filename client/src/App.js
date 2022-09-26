42ff60a8b7864bd02c95963ef6aa2baf683858e3
Sarah Gerrard <98355961+LadyBluenotes@users.noreply.github.com>
2022-09-26 10:48:58 -0700

    added files, connected db, started with routes for landing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,32 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 import LandingPage from './pages/landing-page/LandingPage'
-
+import SignIn from './pages/sign-in/SignIn'
+import SignUp from './pages/sign-up/SignUp'
 
 function App() {
   return (
-    <>
-      <LandingPage />
-    </>
+    <Router>
+      <Routes>
+        <Route 
+          path ='/'
+          element={<LandingPage />}
+        />
+        <Route 
+          path ='/signin'
+          element={<SignIn />}
+        />
+        <Route 
+          path ='/signup'
+          element={<SignUp />}
+        />
+      </Routes>
+    </Router>
   );
 }
 
