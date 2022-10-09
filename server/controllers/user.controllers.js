1f3debaaa0976753cba255d562ab2fcaec76d3b1
Sarah Gerrard <98355961+LadyBluenotes@users.noreply.github.com>
2022-10-09 10:28:48 -0700

    user logout created and create user and login user modified

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -1,4 +1,5 @@
-const User = require('../models/User.model')
+const User = require('../models/User.model');
+const bcrypt = require('bcrypt');
 
 const getLogin = (req, res) => {
 
@@ -11,23 +12,21 @@ const loginUser = async (req, res, next) => {
         const user = await User.findOne({ email })
         if (!user) {
             res.status(401).json({
-                message: 'Login was not successful.',
-                error: "Email not found."
+                msg: 'Login was not successful.',
+                error: 'Email not found.'
             })
         } else if (user && (await user.comparePassword(password))){
-            res.json({
-                _id: user._id,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                email: user.email
-            })
-
-        } else {
-
             res.status(200).json({
-                message: "Successful login.",
-                user
+                msg: 'User logged in.',
+                "user": {
+                    _id: user._id,
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    email: user.email,
+                    hashPassword: user.password
+                }
             })
+            // redirect to home page
         }
     } catch(err) {
         res.status(400).json({
@@ -38,14 +37,21 @@ const loginUser = async (req, res, next) => {
 }
 
 const logout = (req, res) => {
-
+    req.session.destroy(function (error) {
+        if (error) next(error);
+        else res.status(200).json({ message: "Succesfully disconnected." });
+      });
 }
 
 const createUser = async (req, res, next) => {
     const { firstName, lastName, email, password } = req.body;
 
+    if ( !email || !password ) {
+        return res.status(401).json({ msg: 'Password and email are required.' })
+    }
+
     if (password.length < 8) {
-        return res.status(400).json({ message: "Please ensure password is a minimum of 8 characters long." })
+        return res.status(400).json({ msg: "Please ensure password is a minimum of 8 characters long." })
       }
 
     const existUser = await User.findOne({ email });
@@ -61,6 +67,8 @@ const createUser = async (req, res, next) => {
             message: "User successfully created.",
             user,
           });
+
+          //redirect to home page
 
     }catch (Error){
         res.status(400).json({
